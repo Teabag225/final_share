015c3768c98a91174e9bbbe6618698767241d6a4
Teabag225 <taewook225@naver.com>
2022-10-26 11:41:49 +0900

    push elements in index.html(subpage), detail_pages

--- app.js
+++ app.js
@@ -9,6 +9,18 @@ app.use('/static', express.static(__dirname + '/static'));
 app.get('/', function(request, response){
     response.render('index')
 });
+    
+
+// dicegame.ejs
+app.get('/dicegame', function(request, response){
+    response.render('dicegame')
+});
+
+// detail_page.ejs 을밀대냉면
+app.get('/detail_page', function(request, response){
+    response.render('detail_page')
+});
+
 
 app.listen(PORT, function(){
     console.log(`Listening on port ${PORT}!`);
