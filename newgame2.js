a703958fd9420d5e20c256bd23f492b7fd1868ac
uj-kim <ninakyj@gmail.com>
2022-10-26 16:38:32 +0900

    fixError: 더이상오류없기를

--- newgame2.js
+++ newgame2.js
@@ -10,6 +10,11 @@ let insInput = document.querySelector(".input");
 const popUp = document.querySelector('.modal');
 const pTxt = popUp.querySelector('p');
 
+let btnYY = document.querySelector('#btnY');
+let btnNN = document.querySelector('#btnN');
+let btnTxtY = document.querySelector('#thisone');
+let btnTxtN = document.querySelector('#notthisone');
+
 
 let cnt = 2;
 function add(){
@@ -126,17 +131,25 @@ function run(){
 
   const imgCard = document.querySelectorAll('.card');
   const inputBorder = document.querySelectorAll('input');
-  const chgImg = document.getElementById('submitBtn');
   const arr = [];
   const txt1 = document.querySelector('.txt1');
-  txt1.style.display = "inline-block";
-  
+
+ console.log(imgCard);
+  // txt1 = txt1 + pointerImg;
   document.querySelector('.add').style.display = "none";
   document.querySelector('.remove').style.display = "none";
-  chgImg.setAttribute('src', './static/image/poker-cards.png');
-  
+  document.querySelector('#submitBtn').classList.remove('upDown');
+  document.querySelector('#submitBtn').addEventListener('click',
+  function(){
+    popUp.style.display="inline-flex";
+    pTxt.innerText = "게임이 실행중입니다.";
+    pTxt.style.textAlign="center";
+    pTxt.style.whiteSpace = "nowrap";
+    document.querySelector('#popBtnY').addEventListener('click', function(){
+      popUp.style.display = "none";
+    });
+  });
   // document.querySelector('.shuffle').style.transform = "translateX(-50px);"
-
   //유저수만큼 카드 보이기 
   for ( var i = 0; i < inputBorder.length; i++ ) {
     if(inputBorder[i].value != ""){
@@ -149,71 +162,111 @@ function run(){
 
     arr[i] = inputBorder[i].value; //빈 배열에 값 삽입
     imgCard[i].style.position = "absolute";
+    txt1.style.display = "inline-block";
     // imgCard[i].style.left = "100px";
   } 
-  else{popUp.style.display="inline-flex";} //User에 값을 입력하지 않을 경우
+  else{popUp.style.display="inline-flex"; //User에 값을 입력하지 않을 경우
         pTxt.innerText = "User 이름을 모두 입력해주세요."
         pTxt.style.textAlign="center";
         pTxt.style.whiteSpace = "nowrap";
         document.querySelector('#popBtnY').addEventListener('click',function(){
           popUp.style.display="none";
-        })
+          location.replace(location.href);
+        // txt1.style.display = "none";
+        })};
 };
   //유저값 랜덤 추출
-  const min = Math.ceil(0);
-  const max = Math.floor(i);
-  const r = Math.floor(Math.random()*(max-min))+min;
-  const rTxt =  arr[r].toString();
-  console.log(imgCard[r]);
+
+ 
+  // const r = Math.floor(Math.random()*(max-min))+min;
+ 
+
   
   // console.log(txt2);
-  const frontImg = imgCard[max-1];
-  const txt2 = imgCard[r].querySelector('.txt2 > p');
+ 
+  
 
-  frontImg.addEventListener('click', function(){
+
+  txt1.addEventListener('click', function(){
+    const frontImg = imgCard[0];
+    frontImg.classList.add('slideUp');
+
+    const r =Math.floor(Math.random() * arr.length);
+    const rTxt =  arr[r].toString();
+    console.log(rTxt);
     
-    imgCard[r].classList.add('slideUp');
-    
+    const txt2 = frontImg.querySelector('.txt2 > p');
     txt1.style.display = "none";
     
   
-  let btnY = document.querySelector('#btnY');
-  let btnN = document.querySelector('#btnN');
-  // cardone.classList.add("slideUp");
-  btnY.style.display = "flex";
-  btnN.style.display = "flex";
-  });
+    let btnY = document.querySelector('.div2_2');
+    let btnN = document.querySelector('.div2_1');
+      
+    // cardone.classList.add("slideUp");
+    btnY.style.display = "inline-block";
+    btnN.style.display = "inline-block";
+    btnYY.style.display = "inline-block";
+    btnNN.style.display = "inline-block";
+    btnTxtY.style.display = "inline-block";
+    btnTxtN.style.display = "inline-block";
   
+});
   //카드 확정 -> 빙글빙글돌면서 확대
-btnY.addEventListener('click', function() {
-  // let btnY = document.querySelector('#btnY');
-  // let btnN = document.querySelector('#btnN');
-
-  btnY.style.display = "none";
  
-  // btnN.style.display = "none";
-  // imgCard[r].style.transform = "translateX(100%) rotateY(-180deg) scale(3)";
-  imgCard[r].classList.add("rotateCard");
-  txt2.innerHTML = `${rTxt}님의 카드`;
-});
-  
-// 카드 취소 -> 다시 원래 자리로 돌려보내기
-btnN.addEventListener('click', function() {
-  imgCard[r].classList.add("slideDown");
+  //이 카드로 할게요(당첨자공개)
+  function yes() {
 
+    btnYY.style.display = "none";
+    btnNN.style.display = "none";
+    btnTxtY.style.display = "none";
+    btnTxtN.style.display = "none";
+   
+    // btnN.style.display = "none";
+    // imgCard[r].style.transform = "translateX(100%) rotateY(-180deg) scale(3)";
+    const r =Math.floor(Math.random() * arr.length);
+    const rTxt =  arr[r].toString();
+    const frontImg = imgCard[0];
+    const txt2 = frontImg.querySelector('.txt2 > p');
+    frontImg.classList.add("rotateCard");
+    txt2.innerHTML = `${rTxt}님의 카드`;
+  };
+
+  //다시뽑을래요
+  function nono() {
+    // console.log(imgCard[r])
+    // frontImg.classList.remove("slideUp");
+    const frontImg = imgCard[0];
+    const txt2 = frontImg.querySelector('.txt2 > p');
+    frontImg.classList.add("slideDown");
+   
+    
+    
+    btnYY.style.display = "none";
+    btnNN.style.display = "none";
+    btnTxtY.style.display = "none";
+    btnTxtN.style.display = "none";
+    
+    function delay(){
+      frontImg.classList.remove("slideUp");
+      frontImg.classList.remove("slideDown");
+      txt1.style.display = "inline-block";
+    }
+    //Animation Remove 지연
+    setTimeout(delay, 500);
+
+    
+    
+  };
+
+  //함수실행
+  btnYY.addEventListener('click', yes);
+  btnTxtY.addEventListener('click',yes);
   
-  btnY.style.display = "none";
-  btnN.style.display = "none";
-  
-  //Animation Remove 지연
-  setTimeout(function(){
-    imgCard[r].classList.remove("slideUp");
-    imgCard[r].classList.remove("slideDown");
-  }, 1000);
-});
+  btnNN.addEventListener('click', nono);
+  btnTxtN.addEventListener('click',nono);
 
 
-}
+};
 
 //카드 셔플 애니메이션
 
@@ -277,121 +330,121 @@ if (! mediaQuery || ! mediaQuery.matches) {
 
 
 //불꽃놀이 애니메이션
-var c = document.getElementById('canvas');
-var ctx = c.getContext("2d");
-console.log(c);
-var cwidth, cheight;
-var shells = [];
-var pass = [];
+// var c = document.getElementById('canvas');
+// var ctx = c.getContext("2d");
+// console.log(c);
+// var cwidth, cheight;
+// var shells = [];
+// var pass = [];
 
-var clrs = ['ff5252', '#ff4081', '#e040fb', '#7c4dff', '#53dff', '#40cdff', '#18ffff', '#64ffda', '#69f0ae', '#b2fff59'];
+// var clrs = ['ff5252', '#ff4081', '#e040fb', '#7c4dff', '#53dff', '#40cdff', '#18ffff', '#64ffda', '#69f0ae', '#b2fff59'];
 
-window.onresize = function(){reset();}
-reset();
-function reset() {
+// window.onresize = function(){reset();}
+// reset();
+// function reset() {
 
-  cwidth = window.innerWidth;
-  cheight = window.innerHeight;
-  c.width = cwidth;
-  c.height = cheight;
+//   cwidth = window.innerWidth;
+//   cheight = window.innerHeight;
+//   c.width = cwidth;
+//   c.height = cheight;
 
-}
+// }
 
 
 
-function newShell(){
-  var left = (Math.random() > 0.5);
-  var shell = {};
-  shell.x = (1*left);
-  shell.y = 1;
-  shell.xoff = (0.01 + Math.random() * 0.007)* (left ? 1: -1);
-  shell.yoff = 0.01 + Math.random() * 0.007;
-  shell.size = Math.random() * 6 + 3;
-  shell.color = colors[Math.floor(Math.random() * clrs.length)];
+// function newShell(){
+//   var left = (Math.random() > 0.5);
+//   var shell = {};
+//   shell.x = (1*left);
+//   shell.y = 1;
+//   shell.xoff = (0.01 + Math.random() * 0.007)* (left ? 1: -1);
+//   shell.yoff = 0.01 + Math.random() * 0.007;
+//   shell.size = Math.random() * 6 + 3;
+//   shell.color = colors[Math.floor(Math.random() * clrs.length)];
 
-  shells.push(shell);
-}
+//   shells.push(shell);
+// }
 
-function newPass(shell){
-  var passCount = Math.ceil(Math.pow(shell.size, 2) * Math.PI);
+// function newPass(shell){
+//   var passCount = Math.ceil(Math.pow(shell.size, 2) * Math.PI);
 
-  for (i = 0; i < passCount ; i++){
-    var pas = {};
-    pas.x = shell.x * cwidth;
-    pas.y = shell.y * cheight;
+//   for (i = 0; i < passCount ; i++){
+//     var pas = {};
+//     pas.x = shell.x * cwidth;
+//     pas.y = shell.y * cheight;
 
-    var a= Math.random() * 4;
-    var s = Math.random() * 10;
+//     var a= Math.random() * 4;
+//     var s = Math.random() * 10;
 
-    pas.xoff = s * Math.sin((5-a) * (Math.PI / 2));
-    pas.yoff = s * Math.sin(a * (Math.PI / 2));
+//     pas.xoff = s * Math.sin((5-a) * (Math.PI / 2));
+//     pas.yoff = s * Math.sin(a * (Math.PI / 2));
 
-    pas.color = shell.color;
-    pas.size = Math.sqrt(shell.size);
+//     pas.color = shell.color;
+//     pas.size = Math.sqrt(shell.size);
 
-    if(pass.length < 1000){ pass.push(pas);}
+//     if(pass.length < 1000){ pass.push(pas);}
 
-  }
-}
+//   }
+// }
 
-var lastRun = 0;
-Run();
-function Run() {
+// var lastRun = 0;
+// Run();
+// function Run() {
 
-var dt = 1;
-if(lastRun != 0){
-  dt = Math.min(50, (performance.now() - lastRun));
-}
-lastRun = performance.now();
+// var dt = 1;
+// if(lastRun != 0){
+//   dt = Math.min(50, (performance.now() - lastRun));
+// }
+// lastRun = performance.now();
 
-ctx.fillstyle = "rgba(0,0,0,0.25)";
-ctx.fillRect(0,0,cwidth,cheight);
+// ctx.fillstyle = "rgba(0,0,0,0.25)";
+// ctx.fillRect(0,0,cwidth,cheight);
 
-if((shells.length < 10) && (Math.random() > 0.96)) {
-  newShell();
-}
-for (let ix in shells){
-  var shell = shells[ix];
+// if((shells.length < 10) && (Math.random() > 0.96)) {
+//   newShell();
+// }
+// for (let ix in shells){
+//   var shell = shells[ix];
 
-  ctx.beginPath();
-  ctx.arc(shell.x * cwidth, shell.y * cheight, shell.size, 0, 2 * Math.PI);
+//   ctx.beginPath();
+//   ctx.arc(shell.x * cwidth, shell.y * cheight, shell.size, 0, 2 * Math.PI);
 
-  ctx.fillstyle = shell.color;
-  ctx.fill();
+//   ctx.fillstyle = shell.color;
+//   ctx.fill();
 
-  shell.x -= shell.xoff;
-  shell.y -= shell.yoff;
-  shell.xoff -= (shell.xoff * dt * 0.001);
-  shell.yoff -= ((shell.yoff + 0.2)*dt * 0.00005);
+//   shell.x -= shell.xoff;
+//   shell.y -= shell.yoff;
+//   shell.xoff -= (shell.xoff * dt * 0.001);
+//   shell.yoff -= ((shell.yoff + 0.2)*dt * 0.00005);
 
-  if (shell.yoff < -0.005){
-    newPass(shell);
-    shells.splice(ix, 1);
-  }
-}
+//   if (shell.yoff < -0.005){
+//     newPass(shell);
+//     shells.splice(ix, 1);
+//   }
+// }
 
-  for (let ix in pass){
-    var pas = pass[ix];
+//   for (let ix in pass){
+//     var pas = pass[ix];
 
-    ctx.beginPath();
-    ctx.arc(pas.x, pas.y, pas.size, 0,2 * Math.PI);
-    ctx.fillstyle = pas.color;
-    ctx.fill();
+//     ctx.beginPath();
+//     ctx.arc(pas.x, pas.y, pas.size, 0,2 * Math.PI);
+//     ctx.fillstyle = pas.color;
+//     ctx.fill();
 
-    pas.x -= pas.xoff;
-    pas.y -= pas.yoff;
+//     pas.x -= pas.xoff;
+//     pas.y -= pas.yoff;
 
-    pas.xoff -= (pas.xoff * dt * 0.001);
-    pas.yoff -= ((pas.yoff + 5) * dt * 0.00005);
-    pas.size -= (dt * 0.002 * Math.random());
+//     pas.xoff -= (pas.xoff * dt * 0.001);
+//     pas.yoff -= ((pas.yoff + 5) * dt * 0.00005);
+//     pas.size -= (dt * 0.002 * Math.random());
 
-    if ((pas.y > cheight) || (pas.y < -50)|| (pas.size <= 0)){
-      pass.splice(ix, 1);
-    }
-  }
+//     if ((pas.y > cheight) || (pas.y < -50)|| (pas.size <= 0)){
+//       pass.splice(ix, 1);
+//     }
+//   }
 
-  requestAnimationFrame(Run);
-}
+//   requestAnimationFrame(Run);
+// }
 
 
 
