6d4b17f21ec2def43543d852dda9724da58388df
uj-kim <ninakyj@gmail.com>
2022-10-26 22:54:19 +0900

    add: 주석처리

--- newgame2.js
+++ newgame2.js
@@ -1,5 +1,5 @@
-  
-//유저 추가하기
+// 1. 변수 선언
+// USER 추가/삭제 관련
 let card3 = document.querySelectorAll(".noshow")[0];
 let card4 = document.querySelectorAll(".noshow")[1];
 let card5 = document.querySelectorAll(".noshow")[2];
@@ -10,326 +10,337 @@ let insInput = document.querySelector(".input");
 const popUp = document.querySelector('.modal');
 const pTxt = popUp.querySelector('p');
 
+// 카드뽑기 당첨자 관련
 let btnYY = document.querySelector('#btnY');
 let btnNN = document.querySelector('#btnN');
 let btnTxtY = document.querySelector('#thisone');
 let btnTxtN = document.querySelector('#notthisone');
 
-
+//기본값(기본유저수 = 2)
 let cnt = 2;
+
+// 2. 유저추가 기능
 function add(){
-  let userLabel1 = document.createElement('label');
-  userLabel1.id = 'label3';
-  let userLabel2= document.createElement('label');
-  userLabel2.id = 'label4';
-  let userLabel3 = document.createElement('label');
-  userLabel3.id = 'label5';
-  let userLabel4 = document.createElement('label');
-  userLabel4.id = 'label6';
-  let userInput1 = document.createElement('input');
-  userInput1.id = 'user3';
-  userInput1.setAttribute('type','text');
-  let userInput2 = document.createElement('input');
-  userInput2.id = 'user4';
-  userInput2.setAttribute('type','text');
-  let userInput3 = document.createElement('input');
-  userInput3.id = 'user5';
-  userInput3.setAttribute('type','text');
-  let userInput4 = document.createElement('input');
-  userInput4.id = 'user6';
-  userInput4.setAttribute('type','text');
+let userLabel1 = document.createElement('label');
+userLabel1.id = 'label3';
+let userLabel2= document.createElement('label');
+userLabel2.id = 'label4';
+let userLabel3 = document.createElement('label');
+userLabel3.id = 'label5';
+let userLabel4 = document.createElement('label');
+userLabel4.id = 'label6';
+let userInput1 = document.createElement('input');
+userInput1.id = 'user3';
+userInput1.setAttribute('type','text');
+let userInput2 = document.createElement('input');
+userInput2.id = 'user4';
+userInput2.setAttribute('type','text');
+let userInput3 = document.createElement('input');
+userInput3.id = 'user5';
+userInput3.setAttribute('type','text');
+let userInput4 = document.createElement('input');
+userInput4.id = 'user6';
+userInput4.setAttribute('type','text');
 
-  if (card3.classList.contains("show") == false) {
-    card3.classList.add("show");
-    const user3 = document.createTextNode(" User3 ");
-    userLabel1.appendChild(user3);
-    insInput.append(userLabel1, userInput1);
-    //유저 추가
-    cnt++; //유저 숫자 카운트
-  } else if (card4.classList.contains("show") == false) {
-    card4.classList.add("show");
-    const user4 = document.createTextNode(" User4 ");
-    userLabel2.appendChild(user4);
-    insInput.append(userLabel2, userInput2);
-    cnt++;
-  } else if (card5.classList.contains("show") == false) {
-    card5.classList.add("show");
-    const user5 = document.createTextNode("User5 ");
-    userLabel3.appendChild(user5);
-    insInput.append(userLabel3, userInput3);
+if (card3.classList.contains("show") == false) {
   
-    cnt++;
-  } else if (card6.classList.contains("show") == false) {
-    card6.classList.add("show");
-    const user6 = document.createTextNode(" User6 ");
-    userLabel4.appendChild(user6);
-    insInput.append(userLabel4, userInput4);
-    cnt++;
+  card3.classList.add("show");
+  const user3 = document.createTextNode(" User3 ");
+  userLabel1.appendChild(user3);
+  insInput.append(userLabel1, userInput1);
 
-  } else {
-    popUp.style.display="inline-flex";
-    pTxt.innerText = "User는 최대 6명까지 가능합니다.";
-    pTxt.style.textAlign="center";
-    pTxt.style.whiteSpace = "nowrap";
-    document.querySelector('#popBtnY').addEventListener('click',function(){
-    popUp.style.display="none";
-    });
-    }
-}
+  cnt++;
 
-//유저 제거하기
-function remove() {
-  const userInput1 = document.querySelector('#user3');
-  const userInput2 = document.querySelector('#user4');
-  const userInput3 = document.querySelector('#user5');
-  const userInput4 = document.querySelector('#user6');
-  const userLabel1 = document.querySelector('#label3');
-  const userLabel2 = document.querySelector('#label4');
-  const userLabel3 = document.querySelector('#label5');
-  const userLabel4 = document.querySelector('#label6');
+} else if (card4.classList.contains("show") == false) {
 
-  if (card6.classList.contains("show") == true) {
-    card6.classList.remove("show"); // 유저 제거
-    userLabel4.parentNode.removeChild(userLabel4);
-    userInput4.parentNode.removeChild(userInput4);
-    // card6.childNodes.remove();
+  card4.classList.add("show");
+  const user4 = document.createTextNode(" User4 ");
+  userLabel2.appendChild(user4);
+  insInput.append(userLabel2, userInput2);
 
-    cnt--; //유저 숫자 카운트
-  } else if (card5.classList.contains("show") == true) {
-    card5.classList.remove("show");
-    userLabel3.parentNode.removeChild(userLabel3);
-    userInput3.parentNode.removeChild(userInput3);
-   
-    cnt--;
-  } else if (card4.classList.contains("show") == true) {
-    card4.classList.remove("show");
-    userLabel2.parentNode.removeChild(userLabel2);
-    userInput2.parentNode.removeChild(userInput2);
-    
-    cnt--;
-  } else if (card3.classList.contains("show") == true) {
-    card3.classList.remove("show");
-    userLabel1.parentNode.removeChild(userLabel1);
-    userInput1.parentNode.removeChild(userInput1);
-    
-    cnt--;
+  cnt++;
 
-  } else {
-    popUp.style.display="inline-flex";
-    pTxt.innerText = "User는 최소 2명이 필요합니다.";
-    pTxt.style.textAlign="center";
-    pTxt.style.whiteSpace = "nowrap";
-    document.querySelector('#popBtnY').addEventListener('click',function(){
-    popUp.style.display="none";
-    });
+} else if (card5.classList.contains("show") == false) {
+
+  card5.classList.add("show");
+  const user5 = document.createTextNode("User5 ");
+  userLabel3.appendChild(user5);
+  insInput.append(userLabel3, userInput3);
+
+  cnt++;
+
+} else if (card6.classList.contains("show") == false) {
+  card6.classList.add("show");
+  const user6 = document.createTextNode(" User6 ");
+  userLabel4.appendChild(user6);
+  insInput.append(userLabel4, userInput4);
+
+  cnt++;
+
+} else {
+  popUp.style.display="inline-flex";
+  pTxt.innerText = "User는 최대 6명까지 가능합니다.";
+  pTxt.style.textAlign="center";
+  pTxt.style.whiteSpace = "nowrap";
+  document.querySelector('#popBtnY').addEventListener('click',function(){
+  popUp.style.display="none";
+  });
   }
 }
 
+//3. 유저 제거
+function remove() {
+//add()에서 생성된 user별 input박스에 id 부여 > 선택 > 제거
+const userInput1 = document.querySelector('#user3');
+const userInput2 = document.querySelector('#user4');
+const userInput3 = document.querySelector('#user5');
+const userInput4 = document.querySelector('#user6');
+const userLabel1 = document.querySelector('#label3');
+const userLabel2 = document.querySelector('#label4');
+const userLabel3 = document.querySelector('#label5');
+const userLabel4 = document.querySelector('#label6');
 
-//게임시작 (Start button_#submitBtn)
+if (card6.classList.contains("show") == true) {
+
+  card6.classList.remove("show");
+  userLabel4.parentNode.removeChild(userLabel4);
+  userInput4.parentNode.removeChild(userInput4);
+
+  cnt--;
+
+} else if (card5.classList.contains("show") == true) {
+
+  card5.classList.remove("show");
+  userLabel3.parentNode.removeChild(userLabel3);
+  userInput3.parentNode.removeChild(userInput3);
+
+  cnt--;
+
+} else if (card4.classList.contains("show") == true) {
+
+  card4.classList.remove("show");
+  userLabel2.parentNode.removeChild(userLabel2);
+  userInput2.parentNode.removeChild(userInput2);
+  
+  cnt--;
+
+} else if (card3.classList.contains("show") == true) {
+
+  card3.classList.remove("show");
+  userLabel1.parentNode.removeChild(userLabel1);
+  userInput1.parentNode.removeChild(userInput1);
+  
+  cnt--;
+
+} else {
+  popUp.style.display="inline-flex";
+  pTxt.innerText = "User는 최소 2명이 필요합니다.";
+  pTxt.style.textAlign="center";
+  pTxt.style.whiteSpace = "nowrap";
+  document.querySelector('#popBtnY').addEventListener('click',function(){
+  popUp.style.display="none";
+  });
+}
+}
+
+
+//4. 게임실행 (Start button_#submitBtn)
 function run(){
 
-  const imgCard = document.querySelectorAll('.card');
-  const inputBorder = document.querySelectorAll('input');
-  const arr = [];
-  const txt1 = document.querySelector('.txt1');
+const imgCard = document.querySelectorAll('.card');
+const inputBorder = document.querySelectorAll('input');
+const txt1 = document.querySelector('.txt1');
+const arr = [];
 
- console.log(imgCard);
-  // txt1 = txt1 + pointerImg;
-  document.querySelector('.add').style.display = "none";
-  document.querySelector('.remove').style.display = "none";
-  document.querySelector('#submitBtn').classList.remove('upDown');
-  document.querySelector('#submitBtn').addEventListener('click',
-  function(){
-    popUp.style.display="inline-flex";
-    pTxt.innerText = "게임이 실행중입니다.";
-    pTxt.style.textAlign="center";
-    pTxt.style.whiteSpace = "nowrap";
-    document.querySelector('#popBtnY').addEventListener('click', function(){
-      popUp.style.display = "none";
-    });
+
+document.querySelector('.add').style.display = "none"; //유저추가버튼 안보이게
+document.querySelector('.remove').style.display = "none"; //유저제거버튼 안보이게
+document.querySelector('#submitBtn').classList.remove('upDown'); // 스타트버튼에 있는 애니메이션 제거
+
+// 게임이 시작 되었는데 시작버튼을 또 눌렀을 경우 경고창 팝업
+document.querySelector('#submitBtn').addEventListener('click',
+function(){
+  popUp.style.display="inline-flex";
+  pTxt.innerText = "게임이 실행중입니다.";
+  pTxt.style.textAlign="center";
+  pTxt.style.whiteSpace = "nowrap";
+  document.querySelector('#popBtnY').addEventListener('click', function(){
+    popUp.style.display = "none";
   });
-  // document.querySelector('.shuffle').style.transform = "translateX(-50px);"
-  //유저수만큼 카드 보이기 
-  for ( var i = 0; i < inputBorder.length; i++ ) {
-    if(inputBorder[i].value != ""){
-    inputBorder[i].style.border = '0px solid';  
-    inputBorder[i].style.background = 'none';  
-    inputBorder[i].style.fontFamily = 'HBIOS-SYS';  
-    inputBorder[i].style.color = '#f8e71c';  
-    inputBorder[i].style.fontSize = 'large';  
-    inputBorder[i].style.letterSpacing = '3px';  
-
-    arr[i] = inputBorder[i].value; //빈 배열에 값 삽입
-    imgCard[i].style.position = "absolute";
-    txt1.style.display = "inline-block";
-    // imgCard[i].style.left = "100px";
-  } 
-  else{popUp.style.display="inline-flex"; //User에 값을 입력하지 않을 경우
-        pTxt.innerText = "User 이름을 모두 입력해주세요."
-        pTxt.style.textAlign="center";
-        pTxt.style.whiteSpace = "nowrap";
-        document.querySelector('#popBtnY').addEventListener('click',function(){
-          popUp.style.display="none";
-          location.replace(location.href);
-        // txt1.style.display = "none";
-        })};
-};
-  //유저값 랜덤 추출
-
- 
-  // const r = Math.floor(Math.random()*(max-min))+min;
- 
-
-  
-  // console.log(txt2);
- 
-  
-
-
-  txt1.addEventListener('click', function(){
-    const frontImg = imgCard[0];
-    frontImg.classList.add('slideUp');
-
-    const r =Math.floor(Math.random() * arr.length);
-    const rTxt =  arr[r].toString();
-    console.log(rTxt);
-    
-    const txt2 = frontImg.querySelector('.txt2 > p');
-    txt1.style.display = "none";
-    
-  
-    let btnY = document.querySelector('.div2_2');
-    let btnN = document.querySelector('.div2_1');
-      
-    // cardone.classList.add("slideUp");
-    btnY.style.display = "inline-block";
-    btnN.style.display = "inline-block";
-    btnYY.style.display = "inline-block";
-    btnNN.style.display = "inline-block";
-    btnTxtY.style.display = "inline-block";
-    btnTxtN.style.display = "inline-block";
-  
 });
-  //카드 확정 -> 빙글빙글돌면서 확대
- 
-  //이 카드로 할게요(당첨자공개)
-  function yes() {
 
-    btnYY.style.display = "none";
-    btnNN.style.display = "none";
-    btnTxtY.style.display = "none";
-    btnTxtN.style.display = "none";
-   
-    // btnN.style.display = "none";
-    // imgCard[r].style.transform = "translateX(100%) rotateY(-180deg) scale(3)";
-    const r =Math.floor(Math.random() * arr.length);
-    const rTxt =  arr[r].toString();
-    const frontImg = imgCard[0];
-    const txt2 = frontImg.querySelector('.txt2 > p');
-    frontImg.classList.add("rotateCard");
-    txt2.innerHTML = `${rTxt}님의 카드`;
-  };
 
-  //다시뽑을래요
-  function nono() {
-    // console.log(imgCard[r])
-    // frontImg.classList.remove("slideUp");
-    const frontImg = imgCard[0];
-    const txt2 = frontImg.querySelector('.txt2 > p');
-    frontImg.classList.add("slideDown");
-   
-    
-    
-    btnYY.style.display = "none";
-    btnNN.style.display = "none";
-    btnTxtY.style.display = "none";
-    btnTxtN.style.display = "none";
-    
-    function delay(){
-      frontImg.classList.remove("slideUp");
-      frontImg.classList.remove("slideDown");
-      txt1.style.display = "inline-block";
-    }
-    //Animation Remove 지연
-    setTimeout(delay, 500);
+//유저수만큼 카드 보이기 
+for ( var i = 0; i < inputBorder.length; i++ ) {
+  if(inputBorder[i].value != ""){
+  inputBorder[i].style.border = '0px solid';  
+  inputBorder[i].style.background = 'none';  
+  inputBorder[i].style.fontFamily = 'HBIOS-SYS';  
+  inputBorder[i].style.color = '#f8e71c';  
+  inputBorder[i].style.fontSize = 'large';  
+  inputBorder[i].style.letterSpacing = '3px';  
 
-    
-    
-  };
+  arr[i] = inputBorder[i].value; //빈 배열에 값 삽입
+  imgCard[i].style.position = "absolute";
+  txt1.style.display = "inline-block";
+}
+else{popUp.style.display="inline-flex"; //User에 값을 입력하지 않을 경우
+      pTxt.innerText = "User 이름을 모두 입력해주세요."
+      pTxt.style.textAlign="center";
+      pTxt.style.whiteSpace = "nowrap";
+      document.querySelector('#popBtnY').addEventListener('click',function(){
+        popUp.style.display="none";
+        location.replace(location.href);
+      })};
+};
 
-  //함수실행
-  btnYY.addEventListener('click', yes);
-  btnTxtY.addEventListener('click',yes);
+//CLICK HERE 누르면 실행
+txt1.addEventListener('click', function(){
+  //카드 덱에서 맨 윗장 slideUp
+  const frontImg = imgCard[0];
+  frontImg.classList.add('slideUp');
+
+  // 배열값(유저이름) 랜덤 추출
+  const r =Math.floor(Math.random() * arr.length);
+  const rTxt =  arr[r].toString();
+  console.log(rTxt);
   
-  btnNN.addEventListener('click', nono);
-  btnTxtN.addEventListener('click',nono);
+  // const txt2 = frontImg.querySelector('.txt2 > p');
+
+  //CLICKHERE 텍스트 사라짐
+  txt1.style.display = "none"; 
+  
+  // 당첨자 확인 or 다시뽑기 메시지 노출
+  let btnY = document.querySelector('.div2_2');
+  let btnN = document.querySelector('.div2_1');
+    
+  btnY.style.display = "inline-block";
+  btnN.style.display = "inline-block";
+  btnYY.style.display = "inline-block";
+  btnNN.style.display = "inline-block";
+  btnTxtY.style.display = "inline-block";
+  btnTxtN.style.display = "inline-block";
+
+});
+
+//이 카드로 할게요(당첨자공개)
+function yes() {
+
+  btnYY.style.display = "none";
+  btnNN.style.display = "none";
+  btnTxtY.style.display = "none";
+  btnTxtN.style.display = "none";
+ 
+  const r =Math.floor(Math.random() * arr.length);
+  const rTxt =  arr[r].toString();
+  const frontImg = imgCard[0];
+  const txt2 = frontImg.querySelector('.txt2 > p');
+
+  //빙글빙글 + 확대 애니메이션
+  frontImg.classList.add("rotateCard");
+
+  // 카드에 {당첨자} 텍스트 삽입
+  txt2.innerHTML = `${rTxt}님의 카드`;
+};
+
+//다시뽑을래요
+function nono() {
+
+  const frontImg = imgCard[0];
+  // const txt2 = frontImg.querySelector('.txt2 > p');
+
+  //카드 다시 내려놓기
+  frontImg.classList.add("slideDown");
+ 
+  
+  
+  btnYY.style.display = "none";
+  btnNN.style.display = "none";
+  btnTxtY.style.display = "none";
+  btnTxtN.style.display = "none";
+  
+  //에니메이션 클래스 삭제 + CLICKHERE부터 다시 실행(0.5초후 자동)
+  function delay(){
+    frontImg.classList.remove("slideUp");
+    frontImg.classList.remove("slideDown");
+    txt1.style.display = "inline-block";
+  }
+  //Animation Remove 지연
+  setTimeout(delay, 500);
+};
+
+//함수실행
+btnYY.addEventListener('click', yes);
+btnTxtY.addEventListener('click',yes);
+
+btnNN.addEventListener('click', nono);
+btnTxtN.addEventListener('click',nono);
 
 
 };
 
-//카드 셔플 애니메이션
+//5. 카드 셔플 애니메이션
 
 function shuffle(){
-  
-  cardone.classList.add("shuffle1");
-  cardtwo.classList.add("shuffle2");
-  cardthr.classList.add("shuffle3");
-  cardfour.classList.add("shuffle4");
-  cardfive.classList.add("shuffle5");
-  cardsix.classList.add("shuffle6");
 
-  // setTimeout(function(){
-  //   cardone.classList.remove("shuffle1");
-  //   cardtwo.classList.remove("shuffle2");
-  //   cardthr.classList.remove("shuffle3");
-  //   cardfour.classList.remove("shuffle4");
-  //   cardfive.classList.remove("shuffle5");
-  //   cardsix.classList.remove("shuffle6");
+cardone.classList.add("shuffle1");
+cardtwo.classList.add("shuffle2");
+cardthr.classList.add("shuffle3");
+cardfour.classList.add("shuffle4");
+cardfive.classList.add("shuffle5");
+cardsix.classList.add("shuffle6");
 
-  // }, 5000);
+// setTimeout(function(){
+//   cardone.classList.remove("shuffle1");
+//   cardtwo.classList.remove("shuffle2");
+//   cardthr.classList.remove("shuffle3");
+//   cardfour.classList.remove("shuffle4");
+//   cardfive.classList.remove("shuffle5");
+//   cardsix.classList.remove("shuffle6");
+
+// }, 5000);
 }
 
-// 초기화버튼 (새로고침)
+// 6.초기화버튼 (새로고침)
 function reset() {
-  location.replace(location.href);
+location.replace(location.href);
 }
 
 
 
-//네온효과(h1태그)
+//7. 네온효과(h1태그)
 const colors = gsap.to('h1', {
-  paused: true,
-  duration: 20,
-  repeat: -1,
-  '--hue': 360,
+paused: true,
+duration: 20,
+repeat: -1,
+'--hue': 360,
 })
 
 const doRandom = () => {
-  gsap.timeline()
-    .to('h1', {
-      duration: 0.1,
-      opacity: function(){ return gsap.utils.random(.90, .95) },
-      delay: function(){ return gsap.utils.random(.1, 2) },
-    }).to('h1', {
-      duration: 0.1,
-      opacity: 1,
-      onComplete: function(){
-        doRandom()
-      }
-    })
+gsap.timeline()
+  .to('h1', {
+    duration: 0.1,
+    opacity: function(){ return gsap.utils.random(.90, .95) },
+    delay: function(){ return gsap.utils.random(.1, 2) },
+  }).to('h1', {
+    duration: 0.1,
+    opacity: 1,
+    onComplete: function(){
+      doRandom()
+    }
+  })
 }
 
 const mediaQuery = window.matchMedia("(prefers-reduced-motion: reduce)");
 
 if (! mediaQuery || ! mediaQuery.matches) {
-  colors.play();
-  doRandom();
+colors.play();
+doRandom();
 }
 
 
 
-//불꽃놀이 애니메이션
+//8. 불꽃놀이 애니메이션
 // var c = document.getElementById('canvas');
 // var ctx = c.getContext("2d");
 // console.log(c);
@@ -445,7 +456,6 @@ if (! mediaQuery || ! mediaQuery.matches) {
 
 //   requestAnimationFrame(Run);
 // }
-
 
 
 
